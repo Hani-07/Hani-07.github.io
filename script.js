37001c2018c6535b0154fe78a10978208ede2661
Hani Alnahas <haninahas20@gmail.com>
2022-03-04 13:52:28 +0100

    creating parralax scrolling

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("working");
+const parallax = window.getComputedStyle(document.getElementById('hero'), ':before');
+console.log(parallax)
+window.addEventListener("scroll", function() {
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";    
+    console.log(offset);
+    console.log(parallax);
+})
