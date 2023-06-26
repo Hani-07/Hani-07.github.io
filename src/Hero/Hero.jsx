ec5f876459db1102fcd3d83ef2f5a3419ddb1c56
Hani Alnahas <haninahas20@gmail.com>
2023-06-26 19:40:44 +0200

    graduation update

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = (props) => {
         <section id="hero" className="d-flex flex-column justify-content-center title">
             <div>
             <h1><b>Hani Nahas</b></h1>
-            <p><i>B.Sc. Computer Science, Jacobs University bremen</i></p>
+            <p><i>B.Sc. Computer Science, with minor in Robotics and Intelligent Systems</i></p>
             </div>
         </section>
     )
--- src/Projects/Project.jsx
+++ src/Projects/Project.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Project = (props) => {
     return (
         <div className="feature col">
-            <a href={props.link || "javascript:;"} className="alignleft">
+            <a href={props.link || "javascript:;"} className="alignleft no-style-link">
                 <h2>{props.title}</h2>
             </a>
             <h3 className="alignright text-secondary">{props.date}</h3>
