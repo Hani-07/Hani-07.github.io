33fa5389ada750fe545d2a68cbc459ce8477e3ab
Hani Alnahas <haninahas20@gmail.com>
2024-03-03 18:28:47 +0100

    Updates

---
+++ precache-manifest.df576c4b85abbc711503294ff805e959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0096888138e7e78a2c2",
+    "url": "/static/css/main.9704b65b.chunk.css"
+  },
+  {
+    "revision": "c0096888138e7e78a2c2",
+    "url": "/static/js/main.c0096888.chunk.js"
+  },
+  {
+    "revision": "ab53881aff759a393cf7",
+    "url": "/static/css/1.42e0c971.chunk.css"
+  },
+  {
+    "revision": "ab53881aff759a393cf7",
+    "url": "/static/js/1.ab53881a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5775fb7fe2f4e98eb5ac442f2441b4f4",
+    "url": "/index.html"
+  }
+];
